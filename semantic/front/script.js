980e30c153dd0945b94d6e49562cc85d742915e7
arhemd <mehrad.dadar@gmail.com>
2021-12-06 02:11:16 +0330

    separate css and js file; farsi lorem

---
+++ semantic/front/script.js
@@ -0,0 +1,29 @@
+$(document)
+.ready(function() {
+
+  // fix menu when passed
+  $('.masthead')
+    .visibility({
+      once: false,
+      onBottomPassed: function() {
+        $('.fixed.menu').transition('fade in');
+      }, 
+      onBottomPassedReverse: function() {
+        $('.fixed.menu').transition('fade out');
+      }
+    })
+  ;
+
+  // create sidebar and attach to menu open
+  $('.ui.sidebar')
+    .sidebar('attach events', '.toc.item')
+  ;
+
+  // go dark
+  $('.darken')
+    .bind('click',function(){
+      $('*').toggleClass('inverted');
+    })
+  ;
+})
+;
