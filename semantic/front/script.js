59e7b0e0b21f29db5bc005f1e0638d35dd0411e1
arhemd <mehrad.dadar@gmail.com>
2021-12-07 02:17:26 +0330

    night mode rate limit

--- semantic/front/script.js
+++ semantic/front/script.js
@@ -1,30 +1,28 @@
 $(document)
-.ready(function() {
+  .ready(function () {
 
-  // fix menu when passed
-  $('.masthead')
-    .visibility({
-      once: false,
-      onBottomPassed: function() {
-        $('.fixed.menu')//.toggleClass("inverted");
-      }, 
-      onBottomPassedReverse: function() {
-        $('.fixed.menu')//.toggleClass("inverted");
-      }
-    })
-  ;
+    // fix menu when passed
+    $('.masthead')
+      .visibility({
+        once: false,
+        onBottomPassed: function () {
+          $('.fixed.menu') //.toggleClass("inverted");
+        },
+        onBottomPassedReverse: function () {
+          $('.fixed.menu') //.toggleClass("inverted");
+        }
+      });
 
-  // create sidebar and attach to menu open
-  $('.ui.sidebar')
-    .sidebar('attach events', '.toc.item')
-  ;
+    // create sidebar and attach to menu open
+    $('.ui.sidebar').sidebar('attach events', '.toc.item');
+    
+    let ntm = _.throttle(() => $('.mdark, .mwhite').toggleClass('mdark mwhite'), 600)
+    
+    // go dark
+    $('.darken')
+      .bind('click', function () {
+        ntm();
+      });
 
-  // go dark
-  $('.darken')
-    .bind('click',function(){
-      $('.mdark, .mwhite').toggleClass('mdark mwhite');
-    });
 
-  
-})
-;
+  });
