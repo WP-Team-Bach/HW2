c05c44f0341af743dce1b013bd8fcfe21bffbd0a
arhemd <mehrad.dadar@gmail.com>
2021-12-07 14:59:13 +0330

    cards init

--- semantic/front/script.js
+++ semantic/front/script.js
@@ -15,9 +15,9 @@ $(document)
 
     // create sidebar and attach to menu open
     $('.ui.sidebar').sidebar('attach events', '.toc.item');
-    
+
     let ntm = _.throttle(() => $('.mdark, .mwhite').toggleClass('mdark mwhite'), 600)
-    
+
     // go dark
     $('.darken')
       .bind('click', function () {
